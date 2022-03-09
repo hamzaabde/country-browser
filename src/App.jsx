80d32c3b3ab747d22b14c59769ee0ebad4f005c6
hamzaabde <xamsze1234@gmail.com>
2022-03-09 21:09:14 +0300

    Your branch is ahead of 'origin/v2' by 1 commit.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   src/App.jsx
    
    Untracked files:
    	src/components/ErrorDialog.jsx
    	src/components/GridContainer.jsx
    	src/components/GridItem.jsx
    	src/components/Loading.jsx
    	src/hooks/

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import Container from '@components/Container'
 import Header from '@components/Header'
+import Nav from '@components/Nav'
+import GridContainer from '@components/GridContainer'
+
+import useCountries from '@hooks/useCountries'
 
 function App() {
+	const { data, error } = useCountries()
+
 	return (
 		<Container>
 			<Header />
+			<Nav />
+			<GridContainer data={data} error={error} />
 		</Container>
 	)
 }
