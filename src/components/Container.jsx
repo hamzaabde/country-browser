6803c4d6d0b47f000a9af8cd62d32cd102145abf
hamzaabde <xamsze1234@gmail.com>
2022-03-11 10:01:58 +0300

    changed spacing between landmarks

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,6 +1,8 @@
 export default function Container({ children }) {
 	return (
-		<div className={` min-h-screen bg-light-bg flex flex-col items-stretch`}>
+		<div
+			className={` min-h-screen bg-light-bg flex space-y-6 md:space-y-8 lg:space-y-12 flex-col items-stretch`}
+		>
 			{children}
 		</div>
 	)
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,7 @@ import FilterMenu from '@components/FilterMenu'
 export default function Nav() {
 	return (
 		<div className="max-w-screen-xl w-full mx-auto">
-			<div className="flex flex-col space-y-4 sm:space-y-0 sm:flex-row sm:items-stretch  justify-between mx-4 sm:mx-6 md:mx-8 my-4 xs:my-6 sm:space-x-5">
+			<div className="flex flex-col space-y-4 sm:space-y-0 sm:flex-row sm:items-stretch  justify-between mx-4 sm:mx-6 md:mx-8 sm:space-x-5">
 				<SearchBar />
 				<FilterMenu />
 			</div>
